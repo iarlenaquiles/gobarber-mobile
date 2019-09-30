035b3030988e5e1ef43064ba09951540ec365243
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-30 02:29:21 -0300

    criando a pagina de signup(cadastro)

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,51 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image } from 'react-native';
+import Background from '~/components/Background';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+import logo from '~/assets/logo.png';
 
-// import { Container } from './styles';
+export default function SignUp({ navigation }) {
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
 
-export default function SignUp() {
-  return <View />;
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu nome"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Já tenho uma conta</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
 }
