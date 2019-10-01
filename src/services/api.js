a1f25a8d9dee2f98d17c7dfee6f982acbe2aad47
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-30 23:37:47 -0300

    ajustando a url pra rodar no android via usb

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    'https://my-json-server.typicode.com/rodrigotamura/rocket-shoes-react-native',
+  baseURL: 'https://192.168.11.11:3333',
 });
 
 export default api;
