e4bee3677d05ca3ec2c5881faadbf14dbe85b7e2
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-10-02 23:35:35 -0300

    criando a listagem dos prestadores

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-// import { Container } from './styles';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
 import Background from '~/components/Background';
+import api from '~/services/api';
 
-export default function SelectProvider() {
-  return <Background />;
+export default function SelectProvider({ navigation }) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() =>
+                navigation.navigate('SelectDateTime', { provider })
+              }
+            >
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 SelectProvider.navigationOptions = ({ navigation }) => ({
@@ -16,7 +54,7 @@ SelectProvider.navigationOptions = ({ navigation }) => ({
         navigation.navigate('Dashboard');
       }}
     >
-      <Icon name="chevron-left" size={20} color="#FFF" />
+      <Icon name="chevron-left" size={36} color="#FFF" />
     </TouchableOpacity>
   ),
 });
