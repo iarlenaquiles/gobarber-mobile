9e2fd2a95bfcbb453bbc71f54bfdaa2ea66c4c44
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-10-03 00:21:30 -0300

    criando a tela de selecionar a data

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container } from './styles';
+import Background from '~/components/Background';
+import DateInput from '~/components/DateInput';
 
 export default function SelectDateTime() {
-  return <View />;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <Background>
+      <Container>
+        <DateInput date={date} onChange={setDate} />
+      </Container>
+    </Background>
+  );
 }
+
+SelectDateTime.navigationOptions = ({ navigation }) => ({
+  title: 'Selecione o horário',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={36} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
